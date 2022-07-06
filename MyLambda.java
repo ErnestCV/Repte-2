b5875b60a2f5401a1264601e5442bb271fb55c85
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-07-06 20:18:59 +0200

    Add files via upload

---
+++ MyLambda.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MyLambda {
+    public void checker(int[] nums) {
+        if (nums.length == 1 && nums[0] == 0) {
+            System.out.println("NOT A POSSIBLE OPTION");
+        } else {
+            Arrays.stream(nums).forEach(num -> {
+                System.out.println("input:" + num);
+                System.out.println("isOdd:" + isOdd(num));
+                System.out.println("isPrime:" + isPrime(num));
+                System.out.println("isPalindrome:" + isPalindrome(num) + "\n");
+            });
+        }
+    }
+
+    private boolean isOdd(int num) {
+        return num > 0 && num % 2 != 0;
+    }
+
+    private boolean isPrime(int num) {
+        return num > 0 && IntStream.rangeClosed(2, num / 2).noneMatch(i -> num % i == 0);
+    }
+
+    private boolean isPalindrome(int num) {
+        String str = String.valueOf(num);
+        return num > 0 && IntStream.range(0, str.length() / 2).allMatch(i -> str.charAt(i) == str.charAt(str.length() - i - 1));
+    }
+}
